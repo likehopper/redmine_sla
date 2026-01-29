16e270158829dd1aa6f62990fa2a2199b72f1606
likehopper <vincent@vignolle.com>
2026-01-29 22:18:24 +0100

    Fixing errors caused by AI misuse ;(

--- db/sql_views/sla_view_journal_statuses.sql
+++ db/sql_views/sla_view_journal_statuses.sql
@@ -9,11 +9,10 @@ CREATE OR REPLACE VIEW sla_view_journal_statuses AS
         sla_get_date(first_value(issues.created_on) OVER window_journals) AS issue_created_on,
         sla_get_date(first_value(issues.closed_on) OVER window_journals) AS issue_closed_on,
         sla_get_date(first_value(issues.created_on) OVER window_journals) AS journals_created_on,
-        COALESCE(first_value(journal_details.old_value::inte... window_journals, issues.status_id) AS journal_detail_old_value,
-        COALESCE(first_value(journal_details.old_value::inte... OVER window_journals, issues.status_id) AS journal_detail_value
-    FROM issues
+        COALESCE(first_value(journal_details.old_value::integer) OVER window_journals, issues.status_id) AS journal_detail_old_value,
+        COALESCE(first_value(journal_details.old_value::integer) OVER window_journals, issues.status_id) AS journal_detail_value    FROM issues
     LEFT JOIN journals ON ( issues.id = journals.journalized_id )
-    LEFT JOIN journal_details ON ( journals.id = journal_details...erty LIKE 'attr' AND journal_details.prop_key LIKE 'status_id' )
+    LEFT JOIN journal_details ON ( journals.id = journal_details.journal_id AND journal_details.property LIKE 'attr' AND journal_details.prop_key LIKE 'status_id' )
     WINDOW window_journals AS (PARTITION BY issues.id ORDER BY journal_details.id ASC NULLS LAST )
 ) UNION (
     -- Then add all subsequent status changes recorded in journals,
@@ -26,5 +25,5 @@ CREATE OR REPLACE VIEW sla_view_journal_statuses AS
         journal_details.value::integer AS journal_detail_value
     FROM issues
     INNER JOIN journals ON ( issues.id = journals.journalized_id )
-    INNER JOIN journal_details ON ( journals.id = journal_details...erty LIKE 'attr' AND journal_details.prop_key LIKE 'status_id' )
+    INNER JOIN journal_details ON ( journals.id = journal_details.journal_id AND journal_details.property LIKE 'attr' AND journal_details.prop_key LIKE 'status_id' )
 )
